ce79cba7174c541c59b48c503e23b7da36848fbe
Sergey <blackmoloch.cc@gmail.com>
2023-07-01 16:43:35 +0300

    feature(server): install socket io - change start server

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,22 @@ dotenv.config();
 import path from 'path';
 import express from 'express';
 import cors from 'cors';
+import http from 'http';
+import {Server} from 'socket.io';
+
 import { router } from './routers';
 import { sequelize } from './sequelize';
-
-import cookieParser from 'cookie-parser'
-import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 import { errorMiddleware } from './middlewares/error-middleware';
 
 const app = express();
+const server = new http.Server(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:8080'
+    }
+});
 
 const port = process.env.PORT || 4000;
 
@@ -19,7 +27,7 @@ app.use('/images', express.static(path.join(__dirname, 'files', 'images')))
 
 
 app.use(cors({
-    origin: ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:5500',],
+    origin: ['http://localhost:8080', 'http://localhost:3000', 'http://localhost:5500'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 }));
@@ -29,11 +37,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', router);
 app.use(errorMiddleware);
 
+io.on('connection', (socket) => {
+    console.log({socket})
+})
+
 const start = async() => {
     try {
         await sequelize.authenticate();
         await sequelize.sync();
-        app.listen(port, () => console.log(`SERVER started on PORT :  ${port}`));
+        server.listen(port, () => console.log(`SERVER started on PORT :  ${port}`))
+        // app.listen(port, () => console.log(`SERVER started on PORT :  ${port}`));
     } catch (err) {
         console.log(' -- Start Server Error: ')
         console.log(err)
